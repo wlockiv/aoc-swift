b6eb35b064ec1e71050430756e750b2c382b2eff
walker <walker.b.lockard@gmail.com>
2022-12-19 17:26:30 -0600

    day 12 b

--- Sources/Day12/Day12.swift
+++ Sources/Day12/Day12.swift
@@ -1,10 +1,52 @@
-import Algorithms
 
 public func partOne() {
-    let graph = Graph(rawInput: input)
-    let start = graph.start!
-    let destination = graph.destination!
-    var unvisited: Set<Coordinate> = Set(graph.graph.keys)
+    let heightMap = makeHeightMap(rawInput: input)
+    let (graph, markers) = makeGraph(rawInput: input, markerChars: ["S", "E"])
+    
+    let (shortestPath, _) = findShortestPaths(
+        graph: graph,
+        start: markers["S"]!,
+        pathInvalidator: { heightMap[$0]! - heightMap[$1]! > 1 }
+    )
+
+    let answer = shortestPath[markers["E"]!]!
+    
+    print("Day 12, Part 1 :: \(answer)")
+}
+
+public func partTwo() {
+    let heightMap = makeHeightMap(rawInput: input)
+    let (graph, markers) = makeGraph(rawInput: input, markerChars: ["S", "E"])
+    
+    let (shortestPath, _) = findShortestPaths(
+        graph: graph,
+        start: markers["E"]!,
+        pathInvalidator: { heightMap[$0]! - heightMap[$1]! < -1 }
+    )
+    
+    let allAs = heightMap.filter({ $0.value == Int(exactly: Character("a").asciiValue!) })
+    let answer = shortestPath.filter({ allAs.keys.contains($0.key) }).min(by: { $0.value < $1.value })!.value
+    
+    print("Day 12, Part 2 :: \(answer)")
+}
+
+fileprivate struct Coordinate: Hashable {
+    var x: Int
+    var y: Int
+    
+    init(_ x: Int, _ y: Int) {
+        self.x = x
+        self.y = y
+    }
+}
+
+/// Path finder adapted from Djiksta's path finding algo
+fileprivate func findShortestPaths(
+    graph: [Coordinate: [Coordinate]],
+    start: Coordinate,
+    pathInvalidator: (Coordinate, Coordinate) -> Bool
+) -> (shortesDistances: [Coordinate: Int], previousNodes: [Coordinate: Coordinate]) {
+    var unvisited = Set(graph.keys)
     var shortestPath: [Coordinate: Int] = [:]
     var previousNodes: [Coordinate: Coordinate] = [:]
     
@@ -18,117 +60,93 @@ public func partOne() {
         for node in unvisited {
             if currentMinNode == nil {
                 currentMinNode = node
-                continue
             }
-
+            
             if shortestPath[node]! < shortestPath[currentMinNode!]! {
                 currentMinNode = node
             }
         }
-
-        for neighbor in graph.graph[currentMinNode!]! {
-            var tmpValue = shortestPath[currentMinNode!]! + neighbor.cost
-            if tmpValue < shortestPath[neighbor.coord]! {
-                shortestPath[neighbor.coord] = tmpValue
-                previousNodes[neighbor.coord] = currentMinNode
+        
+        for neighbor in graph[currentMinNode!]! {
+            if pathInvalidator(neighbor, currentMinNode!) {
+                continue
+            }
+            
+            let tmpValue = shortestPath[currentMinNode!]! + 1
+            if tmpValue < shortestPath[neighbor]! {
+                shortestPath[neighbor] = tmpValue
+                previousNodes[neighbor] = currentMinNode
             }
         }
-
+        
         unvisited.remove(currentMinNode!)
     }
-
-    print(shortestPath[destination]!)
+    
+    return (shortestPath, previousNodes)
 }
 
-public func partTwo() {
+fileprivate func makeHeightMap(rawInput: String) -> [Coordinate: Int] {
+    let inputLines = rawInput.split(separator: "\n")
+    var heightMap: [Coordinate: Int] = [:]
     
-}
-
-fileprivate struct Graph {
-    let xMax: Int
-    let yMax: Int
-    var graph: [Coordinate: [Edge]]
-    var start: Coordinate?
-    var destination: Coordinate?
-    
-    
-    init(rawInput: String, maxStepUp: Int = 1) {
-        let inputLines = rawInput.split(separator: "\n")
-        self.xMax = inputLines[0].count - 1
-        self.yMax = inputLines.count - 1
-        
-        var heightMap: [Coordinate: Int] = [:]
-        for (y, inputLine) in rawInput.split(separator: "\n").enumerated() {
-            for (x, char) in inputLine.enumerated() {
-                let thisCoord = Coordinate(x, y)
-                if char == "E" {
-                    self.destination = thisCoord
-                    let value = Int(exactly: Character("z").asciiValue!)!
-                    heightMap[thisCoord] = value
-                    continue
-                }
-                
-                if char == "S" {
-                    self.start = thisCoord
-                    let value = Int(exactly: Character("a").asciiValue!)!
-                    heightMap[thisCoord] = value
-                    continue
-                }
-                    
-                heightMap[thisCoord] = Int(exactly: char.asciiValue!)!
+    for (y, line) in inputLines.enumerated() {
+        for (x, rawChar) in line.enumerated() {
+            var char: Character = rawChar
+            if rawChar == "S" {
+                char = "a"
+            } else if rawChar == "E" {
+                char = "z"
             }
-        }
-        
-        var tmpGraph: [Coordinate: [Edge]] = [:]
-        for (coord, height) in heightMap {
-            let neighbors = Graph.getNeighborCoords(of: coord, xMax: self.xMax, yMax: self.yMax)
-                        
-            for neighbor in neighbors {
-                let cost = heightMap[neighbor]! - height
-                
-                if cost <= maxStepUp {
-                    /// Cost is statically 1 since height has no effect technically
-                    let edge = Edge(coord: neighbor, cost: 1)
-                    tmpGraph[coord, default: []].append(edge)
-                }
-            }
-        }
-        self.graph = tmpGraph
-    }
-    
-    static func getNeighborCoords(of coord: Coordinate, xMax: Int, yMax: Int) -> [Coordinate] {
-        var neighbors: [Coordinate] = []
-        let neighborOffsets = [
-            "up": (0, -1),
-            "right": (1, 0),
-            "down": (0, 1),
-            "left": (-1, 0),
-        ]
-        
-        for offset in neighborOffsets {
-            let x = coord.x + offset.value.0
-            let y = coord.y + offset.value.1
             
-            if (0...xMax).contains(x) && (0...yMax).contains(y) {
-                neighbors.append(Coordinate(x, y))
+            let height = Int(exactly: char.asciiValue!)!
+            heightMap[Coordinate(x, y)] = height
+        }
+    }
+    
+    return heightMap
+}
+
+fileprivate func makeGraph(rawInput: String, markerChars: [Character]) -> (graph: [Coordinate: [Coordinate]], markers: [Character: Coordinate]) {
+    let inputLines = rawInput.split(separator: "\n")
+    let xMax = inputLines[0].count - 1
+    let yMax = inputLines.count - 1
+    
+    var graph: [Coordinate: [Coordinate]] = [:]
+    var markers: [Character: Coordinate] = [:]
+    
+    for (y, line) in inputLines.enumerated() {
+        for (x, char) in line.enumerated() {
+            let coord = Coordinate(x, y)
+            if markerChars.contains(char) {
+                markers[char] = coord
             }
+            
+            graph[coord] = getNeighbors(coord: coord, xMax: xMax, yMax: yMax)
+        }
+    }
+    
+    return (graph, markers)
+}
+
+fileprivate func getNeighbors(coord: Coordinate, xMax: Int, yMax: Int) -> [Coordinate] {
+    /// up, right, down, left
+    let offsets = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    var neighbors: [Coordinate] = []
+    
+    for offset in offsets {
+        let newX = coord.x + offset.0
+        let newY = coord.y + offset.1
+        
+        if newX < 0 || newY < 0 {
+            continue
         }
         
-        return neighbors
+        if newX > xMax || newY > yMax {
+            continue
+        }
+        
+        neighbors.append(Coordinate(newX, newY))
     }
-}
-
-fileprivate struct Coordinate: Hashable {
-    var x: Int
-    var y: Int
     
-    init(_ x: Int, _ y: Int) {
-        self.x = x
-        self.y = y
-    }
-}
-
-fileprivate struct Edge {
-    let coord: Coordinate
-    let cost: Int
+    return neighbors
 }
